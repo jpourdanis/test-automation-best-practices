71c547663a3944c797a40626009ad094da10a40e
“pourdanisj” <pourdanisj@vmware.com>
2026-03-02 17:11:37 +0200

    fix the failing test on purpose.

--- e2e/tests/a11y.spec.ts
+++ e2e/tests/a11y.spec.ts
@@ -58,7 +58,7 @@ test.describe("Accessibility Tests", () => {
     const contrastViolations = accessibilityScanResults.violations.filter(
       (v) => v.id === "color-contrast"
     );
-    expect(contrastViolations).toEqual([1]);
+    expect(contrastViolations).toEqual([]);
   });
 });
 
@@ -98,7 +98,7 @@ test.describe("i18n Accessibility Tests", () => {
 
       // Run Axe to check for accessibility violations in the translated state
       const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-      expect(accessibilityScanResults.violations).toEqual([1]);
+      expect(accessibilityScanResults.violations).toEqual([]);
     });
   }
 });
