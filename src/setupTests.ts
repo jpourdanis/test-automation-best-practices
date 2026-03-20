1bf574366881034db68ba1e1f6e7dcff0471776e
“pourdanisj” <pourdanisj@vmware.com>
2026-03-20 11:14:00 +0200

    style: Remove semicolons for consistent code formatting.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,7 +2,7 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom'
 
 // Mock matchMedia if needed (common in some React components)
 Object.defineProperty(window, 'matchMedia', {
@@ -15,6 +15,6 @@ Object.defineProperty(window, 'matchMedia', {
     removeListener: jest.fn(), // deprecated
     addEventListener: jest.fn(),
     removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
+    dispatchEvent: jest.fn()
+  }))
+})
