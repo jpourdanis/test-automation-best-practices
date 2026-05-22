a9fc58a02a9dadf000b6c0755e67f45257e3d0e3
“pourdanisj” <pourdanisj@vmware.com>
2026-05-22 12:14:23 +0300

    fix: update module resolution and shared package build process
    - Use require.resolve for @testing-library/react-native in Jest config for more reliable module resolution
    - Update shared package build to run in correct directory with explicit npm ci before build

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   moduleNameMapper: {
     '^react$': '<rootDir>/node_modules/react',
     '^react-test-renderer$': '<rootDir>/../node_modules/react-test-renderer',
-    '^@testing-library/react-native$': '<rootDir>/node_modules/@testing-library/react-native'
+    '^@testing-library/react-native$': require.resolve('@testing-library/react-native')
   },
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
